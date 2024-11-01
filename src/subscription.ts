b99822a56984d8483c84aa96f15834f2ea13b11b
ahumph <adam.humphreys@upwind.io>
2024-11-01 23:42:18 +0000

    label.val

--- src/subscription.ts
+++ src/subscription.ts
@@ -22,7 +22,7 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
     const postsToCreate = ops.posts.creates
       .filter((create) => {
         let labels = create.record.labels as ComAtprotoLabelDefs.SelfLabels
-        return Array.isArray(labels) && labels.some((label) => label === 'game-audio')
+        return Array.isArray(labels) && labels.values.some((label) => label.val === 'game-audio')
       })
       .map((create) => {
         // map composer-related posts to a db row
